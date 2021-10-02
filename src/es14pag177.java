ad8b7007b18da64dfbe15e376675a5c8e842b838
Baidoc-Alexandru <alexandru.baidoc@itiszuccante.edu.it>
2021-10-02 21:46:47 +0000

    es14pag177

---
+++ src/es14pag177.java
@@ -0,0 +1,12 @@
+public class es14pag177 {
+    public static void main(String[] args){
+        //inserire valore del raggio
+        int raggio;
+        //calcolare circonferenza del cerchio
+        System.out.print(2*raggio*Math.PI);
+        //calcolare area del cerchio
+        System.out.println(raggio*raggio*Math.PI);
+
+    }
+
+}
